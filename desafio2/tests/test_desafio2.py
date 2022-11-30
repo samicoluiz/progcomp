dfb4d35c3541c0328b1b5828830368bb7b7ada53
Luiz Samico <loouiz@gmail.com>
2022-11-30 10:17:05 -0300

    Testes implementados

--- desafio2/tests/test_desafio2.py
+++ desafio2/tests/test_desafio2.py
@@ -1,44 +1,47 @@
+import pytest
 from src.edificio import Edificio
 
 
-def test_mudanca():
+@pytest.fixture
+def edificio_teste() -> Edificio:
+    """Cria um edifício padrão para realizar os testes"""
+    return Edificio(8, [30, 2, 0, 42, 10, 11, 11, 9])
+
+
+def test_mudanca(edificio_teste: Edificio) -> None:
     """"Teste para verificar se a função mudança funciona para um andar não-extremo"""
 
-    predio_teste1 = Edificio(8, [30, 2, 0, 42, 10, 11, 11, 9])
-    predio_teste1.mudanca(3, 5)
-    assert predio_teste1.pessoas_por_andar == [30, 2, 5, 42, 10, 11, 11, 9]
+    edificio_teste.mudanca(3, 5)
+    assert edificio_teste.pessoas_por_andar == [30, 2, 5, 42, 10, 11, 11, 9]
 
 
-def test_mudanca_primeiro_andar():
+def test_mudanca_primeiro_andar(edificio_teste: Edificio) -> None:
     """Teste para verificar se a função mudança funciona para o primeiro andar"""
 
-    predio_teste2 = Edificio(8, [30, 2, 0, 42, 10, 11, 11, 9])
-    predio_teste2.mudanca(1, 100)
-    assert predio_teste2.pessoas_por_andar == [100, 2, 0, 42, 10, 11, 11, 9]
+    edificio_teste.mudanca(1, 100)
+    assert edificio_teste.pessoas_por_andar == [100, 2, 0, 42, 10, 11, 11, 9]
 
 
-def test_mudanca_ultimo_andar():
+def test_mudanca_ultimo_andar(edificio_teste: Edificio) -> None:
     """Teste para verificar se a função mudança funciona para o último andar"""
 
-    predio_teste3 = Edificio(8, [30, 2, 0, 42, 10, 11, 11, 9])
-    predio_teste3.mudanca(8, 63)
-    assert predio_teste3.pessoas_por_andar == [30, 2, 0, 42, 10, 11, 11, 63]
+    edificio_teste.mudanca(8, 63)
+    assert edificio_teste.pessoas_por_andar == [30, 2, 0, 42, 10, 11, 11, 63]
 
 
-def test_censo():
+def test_censo(edificio_teste: Edificio) -> None:
     """Teste para verificar se a função censo funciona para um andar-limite não-extremo"""
-    predio_teste4 = Edificio(8, [30, 2, 0, 42, 10, 11, 11, 9])
-    assert predio_teste4.censo(5) == 84
+
+    assert edificio_teste.censo(5) == 84
 
 
-def test_censo_primeiro_andar():
+def test_censo_primeiro_andar(edificio_teste: Edificio) -> None:
     """Teste para verificar se a função censo funciona quando o andar-limite é o primeiro andar"""
-    predio_teste5 = Edificio(8, [30, 2, 0, 42, 10, 11, 11, 9])
-    assert predio_teste5.censo(1) == 30
+
+    assert edificio_teste.censo(1) == 30
 
 
-def test_censo_ultimo_andar():
+def test_censo_ultimo_andar(edificio_teste: Edificio) -> None:
     """Teste para verificar se a função censo funciona quando o andar-limite é o último andar"""
 
-    predio_teste6 = Edificio(8, [30, 2, 0, 42, 10, 11, 11, 9])
-    assert predio_teste6.censo(8) == 115
+    assert edificio_teste.censo(8) == 115
